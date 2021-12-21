af96733856c02a597fc6a3a1122a7405ab33b481
owalid <othmane.el.ayadi@gmail.com>
2021-12-21 17:39:01 +0100

    add shuttle data

---
+++ static/shuttle_horary.js
@@ -0,0 +1,93 @@
+export default [
+  // {
+  //   "from": {
+  //     "hour": "8:13:00",
+  //     "name": "La Verrière"
+  //   },
+  //   "to": {
+  //     "hour": "8:23:00",
+  //     "name": "Hectar"
+  //   }
+  // },
+  // {
+  //   "from": {
+  //     "hour": "8:33:00",
+  //     "name": "La Verrière"
+  //   },
+  //   "to": {
+  //     "hour": "8:43:00",
+  //     "name": "Hectar"
+  //   }
+  // },
+  // {
+  //   "from": {
+  //     "hour": "9:13:00",
+  //     "name": "La Verrière"
+  //   },
+  //   "to": {
+  //     "hour": "9:23:00",
+  //     "name": "Hectar"
+  //   }
+  // },
+  // {
+  //   "from": {
+  //     "hour": "9:43:00",
+  //     "name": "La Verrière"
+  //   },
+  //   "to": {
+  //     "hour": "9:53:00",
+  //     "name": "Hectar"
+  //   }
+  // },
+  // {
+  //   "from": {
+  //     "hour": "10:13:00",
+  //     "name": "La Verrière"
+  //   },
+  //   "to": {
+  //     "hour": "10:23:00",
+  //     "name": "Hectar"
+  //   }
+  // },
+
+  {
+    "from": {
+      "hour": "17:20:00",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "17:30:00",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "17:45:00",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "17:55:00",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "18:15:00",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "18:25:00",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "18:50:00",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "19:00:00",
+      "name": "La Verrière"
+    }
+  }
+]
