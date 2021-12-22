70459243c9bc517906baafcf0814d01c42110f7d
owalid <othmane.el.ayadi@gmail.com>
2021-12-22 10:12:56 +0100

    init project

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-row>
-      <p>hello world</p>
+      <v-col>
+        <!-- Transport shuttle + train -->
+        <transport-container />
+      </v-col>
+      <v-col>
+        <!-- Weathet -->
+      </v-col>
+      <v-col>
+        <!-- Linkedin -->
+      </v-col>
+      <v-col>
+        <!-- News -->
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import TransportContainer from '~/components/containers/TransportContainer'
 export default {
   name: 'IndexPage',
+  components: { TransportContainer }
 }
 </script>
