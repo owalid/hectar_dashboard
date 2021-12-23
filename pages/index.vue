37780111c389721f3ca9e393b225b3d285327749
owalid <othmane.el.ayadi@gmail.com>
2021-12-23 14:36:02 +0100

    fix typo and css and add icons fontawesome (#16)

--- pages/index.vue
+++ pages/index.vue
@@ -14,11 +14,6 @@
         <hectar-lighting-container />
       </v-col>
     </v-row>
-    <v-row>
-      <v-col>
-        <linkedin-post-container />
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
@@ -26,11 +21,15 @@
 import TransportContainer from '~/components/containers/TransportContainer'
 import HectarLightingContainer from '~/components/containers/HectarLightingContainer'
 import WeatherContainer from '~/components/containers/WeatherContainer'
-import LinkedinPostContainer from '~/components/containers/LinkedinPostContainer'
 import HectarLunchContainer from '~/components/containers/HectarLunchContainer'
 
 export default {
   name: 'IndexPage',
-  components: { TransportContainer, HectarLightingContainer, WeatherContainer, LinkedinPostContainer, HectarLunchContainer }
+  components: {
+    TransportContainer,
+    HectarLightingContainer,
+    WeatherContainer,
+    HectarLunchContainer
+  }
 }
 </script>
