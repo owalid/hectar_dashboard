cd993575ff4e31594dd3e063ff870701c310e833
owalid <othmane.el.ayadi@gmail.com>
2022-02-07 10:38:45 +0100

    add new horary shuttle

--- static/shuttle_horary.js
+++ static/shuttle_horary.js
@@ -52,7 +52,7 @@ export default [
 
   {
     "from": {
-      "hour": "17:20",
+      "hour": "17:19",
       "name": "Hectar"
     },
     "to": {
@@ -62,31 +62,61 @@ export default [
   },
   {
     "from": {
-      "hour": "17:45",
+      "hour": "17:35",
       "name": "Hectar"
     },
     "to": {
-      "hour": "17:55",
+      "hour": "17:46",
       "name": "La Verrière"
     }
   },
   {
     "from": {
-      "hour": "18:15",
+      "hour": "17:51",
       "name": "Hectar"
     },
     "to": {
-      "hour": "18:25",
+      "hour": "18:02",
       "name": "La Verrière"
     }
   },
   {
     "from": {
+      "hour": "18:07",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "18:18",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "18:23",
+      "name": "Hectar"
+    },
+    "to": {
+      "hour": "18:34",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "18:39",
+      "name": "Hectar"
+    },
+    "to": {
       "hour": "18:50",
+      "name": "La Verrière"
+    }
+  },
+  {
+    "from": {
+      "hour": "18:55",
       "name": "Hectar"
     },
     "to": {
-      "hour": "19:00",
+      "hour": "19:05",
       "name": "La Verrière"
     }
   }
